8a3b21a5ea0b5219d030d8b93fe7eb144a8d027c
harshavardhanreddy <harshavardhan.reddy@thoughtworks.com>
2025-09-05 15:59:02 +0530

    First comit

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+# from openpyxl import load_workbook
+
+# Read raw data
+df = pd.read_excel("employees_raw.xlsx", sheet_name="Data")
+
+# Group by Employee ID
+def team_merge(teams):
+    return "Multiple Teams" if len(set(teams)) > 1 else list(teams)[0]
+
+result = df.groupby("Employee ID").agg({
+    "Name": "first",
+    "Region": "first",
+    "Project": lambda x: ",".join(set(x)),
+    "Team": team_merge,
+    "Workspace": lambda x: ",".join(set(x))
+}).reset_index()
+
+# Save cleaned sheet
+result.to_excel("employees_cleaned.xlsx", index=1)
